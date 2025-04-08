fba726f367690089477d5f62a631c8ad225f21ca
Yago Russo <yagrrusso@gmail.com>
2025-04-08 00:45:07 -0300

    feat: editor setup

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,24 +1,22 @@
-import { useState } from "react";
-import { Minus, Plus } from "@phosphor-icons/react";
+import { Minus, Plus } from "@phosphor-icons/react"
+import { useEffect, useState } from "react"
 
 export default function Counter() {
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(5)
+
+  useEffect(() => {
+    console.log(count)
+  }, [count])
 
   return (
-    <div className="flex gap-4 items-center justify-center">
-      <button
-        className="btn btn-sm btn-soft btn-error"
-        onClick={() => setCount(count - 1)}
-      >
+    <div className="flex items-center justify-center gap-4">
+      <button className="btn btn-soft btn-sm btn-error" onClick={() => setCount(count - 1)}>
         <Minus />
       </button>
       <strong className="text-lg font-bold">{count}</strong>
-      <button
-        className="btn btn-sm btn-soft btn-success"
-        onClick={() => setCount(count + 1)}
-      >
+      <button className="btn btn-soft btn-sm btn-success" onClick={() => setCount(count + 1)}>
         <Plus />
       </button>
     </div>
-  );
+  )
 }
