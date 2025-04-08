dd569612a1afe79b5a7bb7f4bdec4e00178b8c3d
Yago Russo <yagrrusso@gmail.com>
2025-04-07 21:54:43 -0300

    feat: first commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Minus, Plus } from "@phosphor-icons/react";
+
+export function Counter() {
+  const [count, setCount] = useState(5);
+
+  return (
+    <div className="flex gap-4 items-center justify-center">
+      <button
+        className="btn btn-sm btn-soft btn-error"
+        onClick={() => setCount(count - 1)}
+      >
+        <Minus />
+      </button>
+      <strong className="text-lg font-bold">{count}</strong>
+      <button
+        className="btn btn-sm btn-soft btn-success"
+        onClick={() => setCount(count + 1)}
+      >
+        <Plus />
+      </button>
+    </div>
+  );
+}
