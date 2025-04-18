4c0ff0b0a12f3fdb95a9be7c04ca02f65703c2ff
Yago Russo <yagrrusso@gmail.com>
2025-04-17 23:22:36 -0300

    fix: test language change on prod

--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -1,8 +1,6 @@
 import { getRelativeLocaleUrl } from "astro:i18n"
+import { useCallback, type HTMLAttributes } from "react"
 
-import type { HTMLAttributes } from "react"
-
-import { cn } from "@/utils/cn"
 import { useTranslation } from "@/utils/i18n"
 
 const BASE_URL = import.meta.env.PUBLIC_BASE_URL
@@ -12,26 +10,26 @@ interface Props extends HTMLAttributes<HTMLSelectElement> {}
 export function LanguageSelect({ className, ...rest }: Props) {
   const { t, locale } = useTranslation()
 
-  const handleRedirect = (newLocale: string) => {
+  const getRedirect = useCallback((newLocale: string) => {
     const fullPath = window.location.pathname
     const parsedPath = fullPath.replace(BASE_URL, "").replace(/^\/(en|pt)/, "")
 
-    const targetUrl = getRelativeLocaleUrl(newLocale, parsedPath)
-    window.location.href = targetUrl
-  }
+    return getRelativeLocaleUrl(newLocale, parsedPath)
+  }, [])
 
   return (
-    <select
-      className={cn("select rounded-xl select-xs", className)}
-      value={locale ?? "unset"}
-      onChange={(ev) => handleRedirect(ev.target.value)}
-      {...rest}
-    >
-      <option value="unset" disabled={true}>
-        {t("selectLanguage")}
-      </option>
-      <option value="en">English (en)</option>
-      <option value="pt">Portugues (pt)</option>
-    </select>
+    <div className="dropdown">
+      <button tabIndex={0} role="button" className="btn rounded-xl border-input text-base-content btn-outline btn-xs">
+        {t("currentLanguage", { lang: locale.toUpperCase() })}
+      </button>
+      <ul tabIndex={0} className="dropdown-content menu z-1 my-2 w-52 rounded-box bg-base-200 p-2 shadow-sm">
+        <li>
+          <a href={getRedirect("pt")}>PT</a>
+        </li>
+        <li>
+          <a href={getRedirect("en")}>EN</a>
+        </li>
+      </ul>
+    </div>
   )
 }
